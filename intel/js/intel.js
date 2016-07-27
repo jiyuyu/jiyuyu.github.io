e6d6cd9d6e35b75736ae88512631976e0b1f157a
jiyuyu <296648521@qq.com>
2016-07-27 17:45:25 +0800

    dele

--- intel/js/intel.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by Administrator on 2016/5/5.
- */
-$("[data-toggle='mask']").hover(function(){
-    $(this).animate({top:'0'},350);
-},function(){
-    $(this).animate({top:'82%'},350);
-});
