028b93d925a86efbbbda0fe786890ca5b28c73a0
jiyuyu <296648521@qq.com>
2016-07-27 17:21:10 +0800

    xiugai

---
+++ intel/js/intel.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Administrator on 2016/5/5.
+ */
+$("[data-toggle='mask']").hover(function(){
+    $(this).animate({top:'0'},350);
+},function(){
+    $(this).animate({top:'82%'},350);
+});
